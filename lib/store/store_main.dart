ab5a8ea6cf0ce62e6c4b26f403e9c6312b033221
leesein-dev <leesein.dev@gmail.com>
2021-11-14 17:31:58 +0900

    complete store

--- lib/store/store_main.dart
+++ lib/store/store_main.dart
@@ -5,6 +5,59 @@ class StorePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Row(
+                children: const [
+                  Text(
+                    "Woman",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(),
+                  Text(
+                    "Kids",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(),
+                  Text(
+                    "Shoes",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(),
+                  Text(
+                    "Bag",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Image.asset(
+                "assets/store/bag.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(
+              height: 2,
+            ),
+            Expanded(
+              child: Image.asset(
+                "assets/store/cloth.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
